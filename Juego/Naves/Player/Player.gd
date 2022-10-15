cfb602285c69729b3ec3f1feee5b45f8e960134e
tanoraf <115107664+tanoraf@users.noreply.github.com>
2022-10-15 19:37:34 -0300

    propiedades
    sonido y enemigo

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -20,6 +20,24 @@ onready var motor_sfx:Motor = $MotorSFX
 onready var colisionador:CollisionShape2D = $CollisionShape2D
 
 ## Metodos
+func controlador_estados(nuevo_estado: int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled",true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(true)
+			Eventos.emit_signal("nave_destruida", global_position)
+			queue_free()
+		_:
+			printerr("Error de estado")
+			estado_actual = nuevo_estado
 func _ready() -> void:
 	controlador_estados(estado_actual)
 
@@ -41,31 +59,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		or event.is_action_released("mover_atras")):
 			motor_sfx.sonido_off()
 
+# warning-ignore:unused_argument
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
+# warning-ignore:unused_argument
 func _process(delta: float) -> void:
 	player_input()
 
 ## Metodos Custom
-func controlador_estados(nuevo_estado: int) -> void:
-	match nuevo_estado:
-		ESTADO.SPAWN:
-			colisionador.set_deferred("disabled", true)
-			canion.set_puede_disparar(false)
-		ESTADO.VIVO:
-			colisionador.set_deferred("disabled", false)
-			canion.set_puede_disparar(true)
-		ESTADO.INVENCIBLE:
-			colisionador.set_deferred("disabled",true)
-		ESTADO.MUERTO:
-			colisionador.set_deferred("disabled", true)
-			canion.set_puede_disparar(true)
-			queue_free()
-		_:
-			printerr("Error de estado")
-			estado_actual = nuevo_estado
+
 func esta_input_activo() -> bool:
 	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
 		return false
@@ -99,3 +103,6 @@ func player_input() -> void:
 func _on_AnimationPlayer_animation_finished(anim_name: String)-> void:
 	if anim_name == "spawn":
 		controlador_estados(ESTADO.VIVO)
+
+func destruir() -> void:
+	controlador_estados(ESTADO.MUERTO)
--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -2,6 +2,9 @@
 class_name Nivel
 extends Node2D
 
+## Atributos Export
+export var explosion:PackedScene = null
+
 ## Metodos
 func _ready() -> void:
 	conectar_seniales()
@@ -9,6 +12,12 @@ func _ready() -> void:
 ## Metodos Custom
 func conectar_seniales() -> void:
 	Eventos.connect("disparo", self, "_on_disparo")
+	Eventos.connect("nave_destruida", self, "_on_nave_destruida")
 
 func _on_disparo(proyectil:Proyectil) -> void:
 	add_child(proyectil)
+
+func _on_nave_destruida(posicion: Vector2) ->void:
+	var new_explosion:Node2D = explosion.instance()
+	new_explosion.global_position = posicion
+	add_child(new_explosion)
